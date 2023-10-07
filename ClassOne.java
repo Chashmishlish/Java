a9f0832510503df5c9163fcd8b108f91b3420797
Shehzadi Alishba Khan <142626257+Chashmishlish@users.noreply.github.com>
2023-10-07 02:27:38 -0700

    Create ClassOne.java

---
+++ ClassOne.java
@@ -0,0 +1,23 @@
+What is class in JAVA 
+  It is a reserved keyword.
+  Jab bhi hum Java me kaam krenge sara ka sara kaaam CLASS ke ander krna prega
+  Class basically apne aap me collection hoti hai FUNCTION ke
+
+Now, What is Function?
+  Function is a set/group of code which performs any task.
+  eg: sum (2,5){
+} or
+  sum (){
+}
+
+How to make a CLASS?
+  ➦Modifiers: A class can be public or has default access.
+  ➦class keyword: The class keyword is used to create a class.
+  ➦Class name: The name must begin with an initial letter (capitalized by convention).
+  ➸Superclass (if any): The name of the class's parent (superclass), if any, preceded by the keyword extends. A class can only extend (subclass) one parent.
+  ➸Interfaces (if any): A comma-separated list of interfaces implemented by the class, if any, preceded by the keyword implements. A class can implement more than one interface.
+  ➦Body: The class body surrounded by braces, { }.
+
+  
+ ➸ Java ka ye standard hota hai jo apki file ka naam ho wohi aap apni class ka naam rkho.
+ ➸ Java ka ye rule hota hai wo humesha "main" naam k function ko phele execute krta hai.
